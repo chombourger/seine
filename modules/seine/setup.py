18e67d69f52fa84fd7870f30a23bd388754d6cac
Cedric Hombourger <chombourger@gmail.com>
2019-11-03 16:08:17 +0530

    modules/seine: debianize
    Signed-off-by: Cedric Hombourger <chombourger@gmail.com>

---
+++ modules/seine/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name="seine",
+    version="0.1",
+    url="https://github.com/chombourger/seine",
+    author="Cedric Hombourger",
+    author_email="chombourger@gmail.com",
+    packages=find_packages(),
+    scripts=['seine'],
+)
