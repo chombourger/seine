dd6c74ee75edfaa2328a394b445c59d682e6e1b4
Cedric Hombourger <chombourger@gmail.com>
2019-11-26 11:38:52 +0100

    seine: use a private podman root directory
    Signed-off-by: Cedric Hombourger <chombourger@gmail.com>

--- seine/utils.py
+++ seine/utils.py
@@ -1,8 +1,24 @@
 
+import os
 import subprocess
 
 class ContainerEngine:
     def hasImage(name):
-        result = subprocess.run(["podman", "image", "exists", name], check=False)
+        result = ContainerEngine.run(["image", "exists", name], check=False)
         return result.returncode == 0
-
+    def _podman_cmd(cmd):
+        home = os.path.expanduser("~")
+        root = os.path.join(home, ".local", "share", "seine")
+        cmd.insert(0, root)
+        cmd.insert(0, "--root")
+        cmd.insert(0, "podman")
+        return cmd
+    def run(cmd, check=False):
+        cmd = ContainerEngine._podman_cmd(cmd)
+        return subprocess.run(cmd, check)
+    def check_output(cmd):
+        cmd = ContainerEngine._podman_cmd(cmd)
+        return subprocess.check_output(cmd)
+    def Popen(cmd, stdin=None, stdout=None, stderr=None):
+        cmd = ContainerEngine._podman_cmd(cmd)
+        return subprocess.Popen(cmd, stdin=stdin, stdout=stdout, stderr=stderr)
