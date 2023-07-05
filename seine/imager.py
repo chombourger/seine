bcea6a2da61a9a5097854f87d33a70514cbed979
Cedric Hombourger <chombourger@gmail.com>
2023-07-05 10:16:51 +0200

    imager: print disk usage after grub is installed into /efi
    Signed-off-by: Cedric Hombourger <chombourger@gmail.com>

--- seine/imager.py
+++ seine/imager.py
@@ -173,12 +173,12 @@ class Imager(Bootstrap):
         script_file.write("\ncd %s\n" % targetdir)
         script_file.write("echo '# Extracting rootfs'\n")
         script_file.write("tar -xf /mnt${tarball}\n")
-        script_file.write("df -h|grep -e '^Filesystem' -e {0}|sed -e 's,{0},/,g'|sed -e 's,^,# ,g' -e 's,//,/,g'\n".format(targetdir))
         script_file.write("update_fstab >etc/fstab\n")
         script_file.write(IMAGER_POST_INSTALL_SCRIPT)
         script_file.write(IMAGER_SELINUX_SETUP_SCRIPT)
         script_file.write(IMAGER_GRUB_INSTALL_SCRIPT)
         script_file.write("copy_bootlets\n")
+        script_file.write("df -h|grep -e '^Filesystem' -e {0}|sed -e 's,{0},/,g'|sed -e 's,^,# ,g' -e 's,//,/,g'\n".format(targetdir))
         script_file.close()
         return script_file.name
 
